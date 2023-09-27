445f4a6ebb24f04bc2198faeac509dec9a15ac07
dbwjddns <142852458+dbwjddns@users.noreply.github.com>
2023-09-27 10:58:16 +0900

    Delete JAVA/js directory

--- JAVA/js/js/background.js
+++
@@ -1,9 +0,0 @@
-const images = ["qw.jpeg", "as.jpeg", "df.jpeg"];
-
-const todayImage = images[Math.floor(Math.random() * images.length)];
-
-const bgImage = document.createElement("img");
-
-bgImage.src = `img/${todayImage}`;
-
-document.body.appendChild(bgImage);
